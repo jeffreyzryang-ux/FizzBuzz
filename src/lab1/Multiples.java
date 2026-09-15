2d11f44e1dc7cb195f4d7a452932022e5aa2f0c8
Jeffrey <jeffrey@example.com>
2026-09-15 16:32:33 -0400

    Complete Java lab exercises

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int count = multiples(1000,3,6);
+        System.out.println(count);
+    }
+
+    static int multiples(int n, int a, int b){
+        int count = 0;
+        int i = 1;
+        while (i < n){
+            if (i % a == 0 || i % b == 0) {
+                count++;
+            }
+
+            i++;
+        }
+        return count;
+    }
+}
